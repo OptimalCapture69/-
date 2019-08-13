9067bf073b16c82b92330258eca9e932cf8b5259
OptimalCapture69 <53558565+OptimalCapture69@users.noreply.github.com>
2019-08-13 17:00:22 +0300

    Add files via upload

---
+++ ap_150409.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+cv2.namedWindow( "size" )
+
+cv2.createTrackbar('min_height', 'size', 0, 500, nothing)
+cv2.createTrackbar('min_width', 'size', 0, 500, nothing)
+
+cv2.createTrackbar('max_height', 'size', 0, 500, nothing)
+
+while 1:
+	min_height = cv2.getTrackbarPos('min_height', 'size')
+	min_width = cv2.getTrackbarPos('min_width', 'size')
+	
+	max_height = cv2.getTrackbarPos('max_height', 'size')
+	max_width = cv2.getTrackbarPos('max_width', 'size')
+	if min_width <= w1 <= max_width and min_height <= h1 <= max_height:
+		print(True)
---
+++ ap_150933.py
@@ -0,0 +1,10 @@
+
+while 1:
+	min_height = 0
+	min_width = 0
+	
+	max_height = 500
+	max_width = 500
+	w1, h1 = 100, 200
+	if min_width <= w1 <= max_width and min_height <= h1 <= max_height:
+		print(True)
